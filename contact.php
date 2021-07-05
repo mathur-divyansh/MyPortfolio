8cc70408825dd55e198360c01bec4ee0bf720a91
Divyansh Mathur <69187107+mathur-divyansh@users.noreply.github.com>
2021-07-05 16:02:45 +0530

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,20 @@
+  
+<?php
+    $name = $_POST["name"];
+    $visitor_email = $_POST['email']; 
+    $message = $_POST['message'];
+    $subject =$_POST['subject'];
+
+    $email_from = 'Divyanshresume@dm.com';
+    $email_subject = 'New Form Submission';
+    $email_body = "UserName: $name.\n"
+                    "Subject: $subject.\n"
+                     "USerEmail: $visitor_email.\n"
+                      "USerMessage: $message,\n";
+    $to ="divyanshmathur@yahoo.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "reply-to: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("location: index.html");
+ 
+ ?>
