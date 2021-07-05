118c2d4ea2bee1d6ce99b5aad6dd2c6f956184d8
Divyansh Mathur <69187107+mathur-divyansh@users.noreply.github.com>
2021-07-05 15:28:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
-  
 <?php
- include_once ("index.html");
+    $name = $_POST["name"];
+    $visitor_email = $_POST['email']; 
+    $message = $_POST['message'];
+    $subject =$_POST['subject'];
+
+    $email_from = 'Divyanshresume';
+    $email_subject = 'New Form Submission';
+    $email_body = "UserName: $name.\n"
+                    "Subject: $subject.\n"
+                     "USerEmail: $visitor_email.\n"
+                      "USerMessage: $message,\n";
+    $to ="divyanshmathur@yahoo.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "reply-to: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("location: index.html");
  ?>
